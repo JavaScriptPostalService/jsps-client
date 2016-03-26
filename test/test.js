b5e888d367063ca6ad2e761b0c708eed6a93eb41
wski <contact.mattdylan@gmail.com>
2016-03-26 00:11:36 -0400

    updated demo with easier example

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 var Shout = new PubSub('ws://localhost:3000/ps');
 
+document.getElementById('send').onclick = function(e){
+  Shout.publish('General', {
+    message: document.getElementById('message').value
+  });
+}
+
 Shout.subscribe('General', msg => {
+  document.getElementById('data').innerHTML += '\n' + JSON.stringify(msg)
   console.log(msg);
-});
-
-
-Shout.publish('General', {
-  message: 'test'
 });
